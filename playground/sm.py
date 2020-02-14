c17c181a34e5705d004d5ea327466189e631ec23
Christopher Mohammed <christophermohammed11@outlook.com>
2020-02-13 20:15:52 -0500

    no comment

---
+++ playground/sm.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+
+df = sm.datasets.macrodata.load_pandas().data
+index = pd.Index(sm.tsa.datetools.dates_from_range('1959Q1', '2009Q3'))
+df.index = index
+
+print(df.head())
+
+plt.plot(df)
+plt.show()
